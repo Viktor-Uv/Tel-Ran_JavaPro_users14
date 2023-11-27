a25e3a339df49ec0441e1327ef8561050036130a
Viktor-Uv <sev-vic@yandex.com>
2023-11-27 19:33:38 +0100

    Initial commit

---
+++ src/main/java/com/example/users14/Users14Application.java
@@ -0,0 +1,13 @@
+package com.example.users14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Users14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Users14Application.class, args);
+    }
+
+}
